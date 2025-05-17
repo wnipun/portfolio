c97087aedf4f07edf7e5a2674569718639fe7f3d
Nipun Perera <nipun5perera@gmail.com>
2025-05-17 10:15:28 +1000

    Fix sidebar items position

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -4,7 +4,7 @@ import LinkedInIcon from "../icons/linkedin"
 
 export default function Sidebar() {
   return (
-    <div className="md:border-r border-gray-100 md:h-screen md:pt-[30%] flex gap-[1rem] flex-row md:flex-col fixed md:relative bottom-0 left-0 justify-center items-center py-[1rem] md:py-0 w-full md:w-auto md:pr-[2rem] bg-white">
+    <div className="md:border-r border-gray-100 md:h-screen md:pt-[30%] flex gap-[1rem] flex-row md:flex-col fixed md:relative bottom-0 left-0 justify-center md:justify-start items-center py-[1rem] md:py-0 w-full md:w-auto md:pr-[2rem] bg-white">
       <Link href="/" title="Home">
         <HomeIcon className="text-primary" />
       </Link>
