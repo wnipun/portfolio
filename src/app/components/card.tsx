c4e0b7229fe24fb6a3e2083f3cbd92dba30b9836
Nipun Perera <nipun5perera@gmail.com>
2025-05-17 10:20:36 +1000

    Refactor Card component to use Link from next/link for navigation

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface CardProps {
     title: string;
@@ -10,16 +11,14 @@ interface CardProps {
 export default function Card({ title, description, image, link }: CardProps) {
   return (
     <div className="flex items-center gap-[1rem] border-b border-b-gray-100 py-[1.5rem]">
-      <div>
+      <div className="shrink-0">
         <Image src={image} alt={title} width={100} height={100} className="w-[100px] h-[100px] object-cover rounded-md"/>
       </div>
       <div>
         <h3 className="text-xl font-bold">{title}</h3>
         <p className="text-gray-600">{description}</p>
       </div>
-      <a href={link} className="text-blue-500 hover:underline">
-        View
-      </a>
+      <Link href={link}>View</Link>
     </div>
   );
 }
