ed57a8b5053e965ccbe118f9ab22c9ccd9c27cff
Nipun Perera <nipun5perera@gmail.com>
2025-05-17 12:09:57 +1000

    Add drop shadow to sidebar on mobile

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -4,7 +4,7 @@ import LinkedInIcon from "../icons/linkedin"
 
 export default function Sidebar() {
   return (
-    <div className="md:border-r border-gray-100 md:h-screen md:pt-[30%] flex gap-[1rem] flex-row md:flex-col fixed md:relative bottom-0 left-0 justify-center md:justify-start items-center py-[1rem] md:py-0 w-full md:w-auto md:pr-[2rem] bg-white">
+    <div className="md:border-r border-gray-100 md:h-screen md:pt-[30%] flex gap-[1rem] flex-row md:flex-col fixed md:relative bottom-0 left-0 justify-center md:justify-start items-center py-[1rem] md:py-0 w-full md:w-auto md:pr-[2rem] bg-white drop-shadow-2xl md:drop-shadow-none border-t md:border-t-0">
       <Link href="/" title="Home">
         <HomeIcon className="text-primary" />
       </Link>
